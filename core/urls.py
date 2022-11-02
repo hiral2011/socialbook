db8c4edf5156724b369683743c5aad8ffad0fda0
hiral2011 <hiral@inventam.com>
2022-11-02 15:10:09 +0530

    after

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('settings', views.Settings, name="settings"),
+    path('upload', views.Upload, name="upload"),
+    path('signup', views.Signup, name="signup"),
+    path('signin', views.Signin, name="signin"),
+    path('logout', views.Logout, name="logout"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,109 @@
-from django.shortcuts import render
+import profile
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='signin')
 def index(request):
+    user_object = request.user
+    # print(user_object)
+    # user_profile = Profile.objects.get(user=user_object)
     return render(request, 'index.html')
+    
+
+@login_required(login_url='signin')
+def Upload(request):
+    return HttpResponse('<h1>Upload View</h1>')
+
+@login_required(login_url='signin')
+def Settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')                   
+    return render(request, 'setting.html', {'user_profile' : user_profile})
+
+def Signup(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2'] 
+        # print(username)
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('Signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('Signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                #log user in and redirect to setting page
+
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+                #creat a profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('settings')
+        else:
+            messages.info(request, 'password not matching')
+            return redirect('Signup')
+            
+    else:
+        return render(request, 'signup.html')
+
+def Signin(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html') 
+
+@login_required(login_url='signin')
+def Logout(request):
+    auth.logout(request)
+    return redirect('signin')
